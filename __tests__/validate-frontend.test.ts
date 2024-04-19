cc699afebd85a918b6812e8b64dc5eb0be907b97
Anders Hassis <anders.hassis@caspeco.se>
2024-04-19 14:06:49 +0000

    ci: add tests (#1)

---
+++ __tests__/validate-frontend.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from "vitest";
+import eslint from "eslint";
+import eslintrcFrontend from "../frontend-vanilla";
+
+import { assertHasEslintError } from "./helper";
+
+describe("validate frontend config", () => {
+  describe("vanilla", () => {
+    it("load config in eslint to validate all rule syntax is correct", async function async() {
+      const cli = new eslint.ESLint({
+        useEslintrc: false,
+        baseConfig: eslintrcFrontend,
+      });
+
+      const result = await cli.lintFiles("./src/frontend.ts");
+      assertHasEslintError(result, "@typescript-eslint/no-unused-vars");
+      assertHasEslintError(result, "no-var");
+      assertHasEslintError(result, "prettier/prettier");
+    });
+  });
+});
