0d58b23f41a91ef952001c9a0cf0c00f0e939363
Anders Hassis <anders.hassis@caspeco.se>
2024-09-10 09:02:33 +0200

    Fixed linting

--- frontend-react.js
+++ frontend-react.js
@@ -1,18 +1,18 @@
-const frontendVanilla = require('./frontend-vanilla');
+const frontendVanilla = require("./frontend-vanilla");
 const reactRefresh = require("eslint-plugin-react-refresh");
 
 /** @type {import('eslint').Linter.Config[]} */
 const flatConfig = [
-  ...frontendVanilla,
-  {
-    files: ['**/*.tsx', '**/*.jsx'],
-    plugins: {
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-    }
-  }
+	...frontendVanilla,
+	{
+		files: ["**/*.tsx", "**/*.jsx"],
+		plugins: {
+			"react-refresh": reactRefresh,
+		},
+		rules: {
+			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+		},
+	},
 ];
 
 module.exports = flatConfig;
--- frontend-vanilla.js
+++ frontend-vanilla.js
@@ -1,45 +1,45 @@
-const tseslint = require('typescript-eslint');
-const eslint = require('@eslint/js');
-const checkFile = require('eslint-plugin-check-file');
+const tseslint = require("typescript-eslint");
+const eslint = require("@eslint/js");
+const checkFile = require("eslint-plugin-check-file");
 const eslintConfigPrettier = require("eslint-config-prettier");
 
 /** @type {import('eslint').Linter.Config[]} */
 const flatConfig = [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['**/*.d.ts', '**/dist/**/*', '**/node_modules/**/*'],
-    linterOptions: {
-      reportUnusedDisableDirectives: 'warn'
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      sourceType: 'module'
-    },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'check-file': checkFile,
-    },
-    rules: {
-      "check-file/filename-naming-convention": [
-        "error",
-        {
-          "**/*.{js,ts,tsx}": "KEBAB_CASE",
-        },
-        {
-          ignoreMiddleExtensions: true,
-        },
-		],
-		"check-file/folder-naming-convention": [
-        "error",
-        {
-          "src/**/": "KEBAB_CASE",
-        },
-      ],
-    },
-  },
-  eslintConfigPrettier
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		ignores: ["**/*.d.ts", "**/dist/**/*", "**/node_modules/**/*"],
+		linterOptions: {
+			reportUnusedDisableDirectives: "warn",
+		},
+		languageOptions: {
+			parser: tseslint.parser,
+			sourceType: "module",
+		},
+		plugins: {
+			"@typescript-eslint": tseslint.plugin,
+			"check-file": checkFile,
+		},
+		rules: {
+			"check-file/filename-naming-convention": [
+				"error",
+				{
+					"**/*.{js,ts,tsx}": "KEBAB_CASE",
+				},
+				{
+					ignoreMiddleExtensions: true,
+				},
+			],
+			"check-file/folder-naming-convention": [
+				"error",
+				{
+					"src/**/": "KEBAB_CASE",
+				},
+			],
+		},
+	},
+	eslintConfigPrettier,
 ];
 
 module.exports = flatConfig;
