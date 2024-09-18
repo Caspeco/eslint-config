2e4363f3c1eca570132c18eb23ec1967b0452757
Anders Hassis <anders.hassis@caspeco.se>
2024-09-18 14:02:37 +0200

    Fix for parserOptions

--- __tests__/fixtures/react/eslint.config.js
+++ __tests__/fixtures/react/eslint.config.js
@@ -1,15 +1,6 @@
 import reactConfig from "./../../../src/frontend-react.js";
 
 const config = [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
   ...reactConfig
 ];
 
