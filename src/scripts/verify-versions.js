cab9d976691981a7a691b68662e4ca387f4a5907
Anders Hassis <anders.hassis@caspeco.se>
2025-01-09 11:56:22 +0100

    Use cli arguments to be less "magical"

--- src/scripts/verify-versions.js
+++ src/scripts/verify-versions.js
@@ -1,11 +1,12 @@
 import fs from "fs";
+
 const packageJson = JSON.parse(fs.readFileSync("package.json", "utf-8"));
-const packagesToVerify = [
-	"eslint-plugin-react",
-	"eslint-plugin-react-hooks",
-	"eslint-plugin-react-refresh",
-	"globals",
-];
+const packagesToVerify = process.argv.slice(2);
+
+if (packagesToVerify.length === 0) {
+	console.error("No packages provided");
+	process.exit(1);
+}
 
 let hasMismatch = false;
 
