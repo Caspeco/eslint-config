2bb6e00645b93446e559516c3b7cea7c57d3f8e6
Anders Hassis <anders.hassis@caspeco.se>
2025-01-09 12:59:06 +0100

    Fixes

--- __tests__/utils/has-eslint-error.ts
+++ __tests__/utils/has-eslint-error.ts
@@ -1,6 +1,7 @@
+import { ESLint } from "eslint";
 import { assert } from "vitest";
 
-export const assertHasEslintError = function (result: any, errorName: any) {
+export const assertHasEslintError = (result: ESLint.LintResult[], errorName: string | null) => {
 	for (const file of result) {
 		for (const error of file.messages) {
 			if (error.ruleId === errorName) {
